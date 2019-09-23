6286c727040759008129f2bae40fc4b3a8bf577e
Nick Gaski <nickgaski@gmail.com>
2019-09-23 10:54:05 -0400

    stable coin

---
+++ contracts/token/ERC20/ERC20stable.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+import "./ERC20Detailed.sol";
+import "../../ownership/Ownable.sol";
+import "./ERC20Mintable.sol";
+import "./ERC20Burnable.sol";
+import "./ERC20Pausable.sol";
+
+contract ERC20Kaleido is ERC20Mintable, ERC20Burnable, ERC20Pausable, ERC20Detailed, Ownable {
+  constructor()
+    Ownable()
+    ERC20Detailed("Stable Currency", "EURO", 0)
+    ERC20Mintable()
+    ERC20Burnable()
+    ERC20Pausable()
+    public {
+    _mint(super.owner(), 1000 * 10**uint(super.decimals()));
+  }
+}
